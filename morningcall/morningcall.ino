0e0e81d9bb1689c1de60d1aa69bb8fd07e979871
小松遥奈 <komatsuna@HarunaKomatsu.local>
2018-06-04 00:53:01 +0900

    マイコン用

---
+++ morningcall/morningcall.ino
@@ -0,0 +1,79 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <AquesTalk.h>
+#include <Wire.h>  // I2CライブラリAquesTalkライブラリ内部で使用するので定義必要
+
+const char* ssid     = "";
+const char* password = "";
+
+const char* host = "http://jewel-maraca.glitch.me";
+AquesTalk atp;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  HTTPClient http;
+
+  http.begin(host);
+  int httpCode = http.GET();
+
+  String result = "";
+
+  if (httpCode < 0) {
+    result = http.errorToString(httpCode);
+  } else if (http.getSize() < 0) {
+    result =  "size is invalid";
+  } else {
+    result = http.getString();
+  }
+
+  http.end();
+  Serial.println(result);
+  atp.SyntheS(result);
+  Serial.println();
+  Serial.println("closing connection");
+}
+
