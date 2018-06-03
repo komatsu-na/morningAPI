b5f6474bd0a4de00aa498dfb62b72ee53b0d8a52
小松遥奈 <komatsuna@HarunaKomatsu.local>
2018-06-03 13:23:19 +0900

    占いAPI使用

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const request = require("request");
 const app = express();
+var util = require('util');
 
 const port = 3000;
+var result;
+
+// 現在日時を取得する
+var now = new Date();
+var nowYear = now.getYear();
+var nowMonths = now.getMonth() + 1;
+var nowDate = now.getDate();
+var date = 2018 + '/0' + 6 + '/0' + 1;
+
+function fortuneAPI() {
+  request.get({
+    url: "http://api.jugemkey.jp/api/horoscope/free/2018/06/01",
+    json:true
+  }, function (err, res, body) {
+    if(!err && res.statusCode == 200) {
+      console.log(body.horoscope[date][8].content);
+      result = body.horoscope[date][8].content;
+      }
+  });
+  }
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  fortuneAPI();
+  res.send(result);
 });
 
 app.listen(port, () => {
